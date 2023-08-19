9742a4b5e3215ebe8fe6702c2b79b8805dffd432
14r14 <rohanramakrish@gmail.com>
2023-08-19 16:08:06 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from "@material-tailwind/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Typography as="p" className="font-dela-gothic font-black text-2xl capitalize">Cal Commit Dashboard</Typography>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+        fontFamily: {
+            "dm-sans": ["DM Sans", "sans-serif"],
+            "dela-gothic": ["Dela Gothic One", "cursive"]
+        }
+    },
+  },
+  plugins: [],
+});

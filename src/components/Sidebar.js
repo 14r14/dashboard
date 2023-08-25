aeedc0cf1f13acf5c8534db273ed67b0f6319d80
14r14 <rohanramakrish@gmail.com>
2023-08-24 17:16:36 -0700

    Finish sidebarV1 for dashboard

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,148 +1,165 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   Typography,
   List,
   ListItem,
   ListItemPrefix,
-  ListItemSuffix,
-  Chip,
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
 } from "@material-tailwind/react";
 import {
-  PresentationChartBarIcon,
-  ShoppingBagIcon,
-  UserCircleIcon,
-  Cog6ToothIcon,
-  InboxIcon,
+  InboxArrowDownIcon,
+  UserIcon,
+  Cog8ToothIcon,
+  PresentationChartLineIcon,
+  ShoppingCartIcon,
+  ChevronDoubleLeftIcon,
   PowerIcon,
-} from "@heroicons/react/24/solid";
-import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
- 
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+
+import logo from "../static/img/Cal Commit Logo.svg";
+import { Link } from "react-router-dom";
+
 export function CCDSidebar() {
-  const [open, setOpen] = React.useState(0);
- 
-  const handleOpen = (value) => {
-    setOpen(open === value ? 0 : value);
-  };
- 
+  const [open, setOpen] = useState(true);
+  const menuItems = [
+    {
+      icon: PresentationChartLineIcon,
+      label: "Placeholder",
+      link: "/placeholder",
+    },
+    { icon: ShoppingCartIcon, label: "Placeholder", link: "/placeholder" },
+    { icon: InboxArrowDownIcon, label: "Placeholder", link: "/placeholder" },
+    { icon: UserIcon, label: "Placeholder", link: "/placeholder" },
+    { icon: Cog8ToothIcon, label: "Placeholder", link: "/placeholder" },
+  ];
+
+  const userMenuItems = [
+    { icon: UserCircleIcon, label: "Username", link: "/settings" },
+    { icon: PowerIcon, label: "Placeholder", link: "/placeholder" },
+  ];
+
+  const openJsx = (
+    <>
+      <Card
+        className={`fixed h-full top-0 left-0 z-40 rounded-r-lg rounded-l-none w-full max-w-[20rem] p-4 shadow-lg shadow-blue-gray-900/5 bg-gradient-to-b from-deep-orange-50 via-deep-orange-100 to-deep-orange-50 translate-x-20`}
+      >
+        <div className="transition-all duration-300 ease-in-out shadow-md cursor-pointer mb-2 bg-white rounded-lg p-2 hover:scale-105 hover:-rotate-12 hover:shadow-lg">
+          <Link
+            to="/"
+            className="flex items-center font-dela-gothic text-2xl text-black"
+          >
+            <img src={logo} alt="Cal Commit" className="h-10 w-auto mr-2" /> Cal
+            Commit
+          </Link>
+        </div>
+        <List>
+          {menuItems.map((item, idx) => (
+            <ListItem key={idx} className="p-0">
+              <Link
+                to={item.link}
+                className={`flex items-center w-full capitalize transition-all duration-300 ease-in-out border-b-0 p-3 hover:bg-calcommit-orange/40 bg-transparent rounded-lg`}
+              >
+                <ListItemPrefix>
+                  <item.icon className="h-5 w-5 text-black" />
+                </ListItemPrefix>
+                <Typography className="mr-auto font-normal text-black text-lg font-dm-sans">
+                  {item.label}
+                </Typography>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
+        <div className="absolute bottom-28 left-auto mr-11">
+          <List className="flex flex-col justify-center items-center border border-calcommit-orange shadow-lg rounded-lg">
+            {userMenuItems.map((item, idx) => (
+              <ListItem
+                key={idx}
+                className="transition-all duration-300 w-full ease-in-out py-3 group hover:bg-calcommit-orange/40 hover:shadow-lg"
+              >
+                <Link
+                  to={item.link}
+                  className={`flex items-center w-full capitalize transition-all duration-300 ease-in-out px-3 border-b-0 rounded-lg rounded-r-full bg-transparent`}
+                >
+                  <ListItemPrefix>
+                    <item.icon className="h-5 w-5 text-black" />
+                  </ListItemPrefix>
+                  <Typography className="mr-auto font-normal text-black text-lg font-dm-sans">
+                    {item.label}
+                  </Typography>
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+        </div>
+      </Card>
+    </>
+  );
+
+  const closedJsx = (
+    <>
+      <Card
+        className={`fixed h-full items-center top-0 left-0 z-40 w-20 rounded-r-lg rounded-l-none px-2 py-4 shadow-lg shadow-blue-gray-900/5 bg-gradient-to-b from-deep-orange-50 via-deep-orange-100 to-deep-orange-50`}
+      >
+        <div className="w-full flex flex-col items-center justify-center">
+          <div className="transition-all w-14 duration-300 ease-in-out justify-center shadow-md cursor-pointer mb-2 bg-white rounded-lg p-2 hover:scale-105 hover:-rotate-12 hover:shadow-lg">
+            <Link to="/" className="flex items-center">
+              <img src={logo} alt="Cal Commit" className="h-10 w-10" />
+            </Link>
+          </div>
+          <List className="flex flex-col justify-center items-center min-w-20 w-full">
+            {menuItems.map((item, idx) => (
+              <ListItem key={idx} className="p-0">
+                <Link
+                  to={item.link}
+                  className={`flex items-center justify-center transition-all duration-300 ease-in-out border-b-0 p-3 rounded-lg rounded-r-full group hover:bg-calcommit-orange hover:shadow-lg bg-transparent`}
+                >
+                  <ListItemPrefix>
+                    <item.icon className="h-5 w-5 text-black" />
+                  </ListItemPrefix>
+                </Link>
+              </ListItem>
+            ))}
+          </List>
+          <div className="absolute bottom-28 left-auto min-w-20 w-full">
+            <List className="flex flex-col justify-center items-center min-w-20 w-full rounded-t-lg rounded-b-lg">
+              {userMenuItems.map((item, idx) => (
+                <ListItem
+                  key={idx}
+                  className="transition-all duration-300 ease-in-out p-2 py-3 group hover:bg-calcommit-orange hover:shadow-lg hover:scale-105 hover:-rotate-6"
+                >
+                  <Link
+                    to={item.link}
+                    className={`flex items-center w-full capitalize transition-all duration-300 ease-in-out px-3 border-b-0 rounded-lg rounded-r-full bg-transparent`}
+                  >
+                    <ListItemPrefix>
+                      <item.icon className="h-5 w-5 text-black" />
+                    </ListItemPrefix>
+                  </Link>
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </div>
+      </Card>
+    </>
+  );
+
   return (
-    <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-      <div className="mb-2 p-4">
-        <Typography variant="h5" color="blue-gray">
-          Sidebar
-        </Typography>
+    <>
+      <div>{open ? openJsx : closedJsx}</div>
+      <div>
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className="absolute left-5 bottom-5 z-50 transition-all duration-300 ease-in-out rounded-lg p-2 bg-transparent shadow-none border border-calcommit-orange hover:scale-110 hover:bg-calcommit-orange"
+        >
+          <ChevronDoubleLeftIcon
+            className={`transiton-transform duration-300 ease-in-out h-5 w-5 text-black ${
+              !open && "rotate-180"
+            }`}
+          />
+        </button>
       </div>
-      <List>
-        <Accordion
-          open={open === 1}
-          icon={
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`mx-auto h-4 w-4 transition-transform ${open === 1 ? "rotate-180" : ""}`}
-            />
-          }
-        >
-          <ListItem className="p-0" selected={open === 1}>
-            <AccordionHeader onClick={() => handleOpen(1)} className="border-b-0 p-3">
-              <ListItemPrefix>
-                <PresentationChartBarIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal">
-                Dashboard
-              </Typography>
-            </AccordionHeader>
-          </ListItem>
-          <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                </ListItemPrefix>
-                Analytics
-              </ListItem>
-              <ListItem>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                </ListItemPrefix>
-                Reporting
-              </ListItem>
-              <ListItem>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                </ListItemPrefix>
-                Projects
-              </ListItem>
-            </List>
-          </AccordionBody>
-        </Accordion>
-        <Accordion
-          open={open === 2}
-          icon={
-            <ChevronDownIcon
-              strokeWidth={2.5}
-              className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
-            />
-          }
-        >
-          <ListItem className="p-0" selected={open === 2}>
-            <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-              <ListItemPrefix>
-                <ShoppingBagIcon className="h-5 w-5" />
-              </ListItemPrefix>
-              <Typography color="blue-gray" className="mr-auto font-normal">
-                E-Commerce
-              </Typography>
-            </AccordionHeader>
-          </ListItem>
-          <AccordionBody className="py-1">
-            <List className="p-0">
-              <ListItem>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                </ListItemPrefix>
-                Orders
-              </ListItem>
-              <ListItem>
-                <ListItemPrefix>
-                  <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                </ListItemPrefix>
-                Products
-              </ListItem>
-            </List>
-          </AccordionBody>
-        </Accordion>
-        <hr className="my-2 border-blue-gray-50" />
-        <ListItem>
-          <ListItemPrefix>
-            <InboxIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Inbox
-          <ListItemSuffix>
-            <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-          </ListItemSuffix>
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            <UserCircleIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Profile
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            <Cog6ToothIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Settings
-        </ListItem>
-        <ListItem>
-          <ListItemPrefix>
-            <PowerIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Log Out
-        </ListItem>
-      </List>
-    </Card>
+    </>
   );
 }
